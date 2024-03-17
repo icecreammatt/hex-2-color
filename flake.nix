a6686bca45835ab9c089bb61996e38ba24fd4c3e
Matt Carrier <icecreammatt@gmail.com>
2024-03-17 12:00:15 -0700

    fix: update flake lock and remove hard coded x86

--- flake.nix
+++ flake.nix
@@ -5,19 +5,24 @@
     utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, utils, naersk }:
-    # utils.lib.eachDefaultSystem (system:
-    utils.lib.eachSystem ["x86_64-linux"] (system:
+  outputs = {
+    self,
+    nixpkgs,
+    utils,
+    naersk,
+  }:
+    utils.lib.eachDefaultSystem (system:
+      #utils.lib.eachSystem ["x86_64-linux"] (system:
       let
-        pkgs = import nixpkgs { inherit system; };
-        naersk-lib = pkgs.callPackage naersk { };
-      in
-      {
+        pkgs = import nixpkgs {inherit system;};
+        naersk-lib = pkgs.callPackage naersk {};
+      in {
         hydraJobs."tester" = self.defaultPackage;
         defaultPackage = naersk-lib.buildPackage ./.;
-        devShell = with pkgs; mkShell {
-          buildInputs = [ cargo rustc rustfmt pre-commit rustPackages.clippy ];
-          RUST_SRC_PATH = rustPlatform.rustLibSrc;
-        };
+        devShell = with pkgs;
+          mkShell {
+            buildInputs = [cargo rustc rustfmt pre-commit rustPackages.clippy];
+            RUST_SRC_PATH = rustPlatform.rustLibSrc;
+          };
       });
 }
